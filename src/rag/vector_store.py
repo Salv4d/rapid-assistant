520b0ac44880042d60a45911233943696ffc87ca
Salvador Oliveira <slsalvadorlucas@gmail.com>
2025-06-23 19:21:11 -0400

    feat: Modify vector store to separe rag from memory

--- src/rag/vector_store.py
+++ src/rag/vector_store.py
@@ -2,8 +2,9 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from src.config import (
+    COLLECTION_NAME_MEMORY,
+    COLLECTION_NAME_RAG,
     VECTORSTORE_PATH,
-    COLLECTION_NAME,
     EMBEDDING_MODEL,
     DEVICE,
     CHUNK_SIZE,
@@ -22,10 +23,17 @@ def get_text_splitter():
         chunk_overlap=CHUNK_OVERLAP
     )
 
-def get_vectorstore():
+def get_rag_vectorstore():
     return Chroma(
         persist_directory=VECTORSTORE_PATH,
-        collection_name=COLLECTION_NAME,
+        collection_name=COLLECTION_NAME_RAG,
+        embedding_function=get_embedding_function()
+    )
+
+def get_memory_vectorstore():
+    return Chroma(
+        persist_directory=VECTORSTORE_PATH,
+        collection_name=COLLECTION_NAME_MEMORY,
         embedding_function=get_embedding_function()
     )
 
@@ -34,6 +42,6 @@ def create_vectorstore_from_documents(documents):
         documents=documents,
         embedding=get_embedding_function(),
         persist_directory=VECTORSTORE_PATH,
-        collection_name=COLLECTION_NAME,
+        collection_name=COLLECTION_NAME_RAG,
     )
 
