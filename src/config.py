ca04aba0dc0ca11ce76f937b0ead2ea2a4d24768
Salvador Oliveira <slsalvadorlucas@gmail.com>
2025-06-23 19:26:29 -0400

    feat: Add separate variables for rag and memory stores

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 import os
 
 VECTORSTORE_PATH = os.getenv("VECTORSTORE_PATH", "data/vectorstore")
-COLLECTION_NAME = os.getenv("VECTORSTORE_COLLECTION", "faq")
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 300))
 CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", 50))
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
 DEVICE = os.getenv("EMBEDDING_DEVICE", "cpu")
 ACTIVE_LLM_PROVIDER = os.getenv("ACTIVE_LLM_PROVIDER", "gemini")
+COLLECTION_NAME_RAG = os.getenv("VECTORSTORE_COLLECTION_RAG", "documents")
+COLLECTION_NAME_MEMORY = os.getenv("VECTORSTORE_COLLECTION_MEMORY", "user_memory")
