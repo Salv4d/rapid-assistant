de5c90c597c457f2590e4f842a8de96a19c0f3f7
Salvador Oliveira <slsalvadorlucas@gmail.com>
2025-06-23 19:22:01 -0400

    feat: Add retrieve memory to init file

--- src/agent/memory/__init__.py
+++ src/agent/memory/__init__.py
@@ -2,4 +2,7 @@ from .base import add_ai_message
 from .base import add_user_message
 from .base import get_history
 from .base import clear
+from .llm_decider import should_store_with_llm
+from .longer_term import retrieve_memory
+from .longer_term import store_memory
 from .prompt import build_contextual_prompt
